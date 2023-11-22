33a8c7867ad1423ce7b8aadefde735d69e31e1d6
jermany17 <leejm5050@naver.com>
2023-11-23 02:01:23 +0900

    Asdfa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,10 +28,15 @@ const Home = () => {
           </Link>
         </div>
         <div className="copyright2">
-          <span>ⓒseat-nullnull</span>
-          <div className="Beta">
-            <span className="material-symbols-outlined beta">all_inclusive</span>
-            <span className="beta2">Beta</span>
+          <div className="cpr21"> 
+            <span>ⓒseat-nullnull</span>
+            <div className="Beta">
+              <span className="material-symbols-outlined beta">all_inclusive</span>
+              <span className="beta2">Beta</span>
+            </div>
+          </div>
+          <div className="cpr22">
+            <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=http%3A%2F%2Fseatnullnull.com%3A3000&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
           </div>
         </div>  
       </Mainborder>
