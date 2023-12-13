5cdade87d7cca855d1f4d3b812e1026e1bf1bbbc
jermany17 <leejm5050@naver.com>
2023-12-13 11:08:14 +0900

    Asf

--- src/pages/Dataplace3.js
+++ src/pages/Dataplace3.js
@@ -33,7 +33,6 @@ const Dataplace3 = () => {
               <span className="ctext">금일(현재 날짜) 시간별 평균 포화도를 표시한 그래프입니다</span>
               <br/>
               <span className="cText">🚨 좌석 포화도 정보 참고사항</span>
-              <span className="ctext">현재 표시되는 데이터는 임의의 값으로</span>
               <span className="ctext">계속해서 예측에 필요한 데이터를 모으는 중입니다 !</span>
               <span className="ctext">데이터가 모이는대로 빠르게 포화도 정보를 제공해드리겠습니다 :)</span>
             </div>
